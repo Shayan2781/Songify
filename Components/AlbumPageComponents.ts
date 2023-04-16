417d63dec61873a7edd9e1e0feffde5e43afde56
Shayan Ahmadizad <99325811+Shayan2781@users.noreply.github.com>
2023-04-16 14:55:13 +0330

    Add files via upload

---
+++ Components/AlbumPageComponents.ts
@@ -0,0 +1,189 @@
+import { Album, Music, Root, Root2 } from "../data/SongFormatter";
+import { htmlToElement, setRequestedColor } from "./HomeComponents";
+import songs from '../data/songs.json';
+import { createPlayPage, getSongIndex } from "./PlayComponents";
+import { addToLikedAlbums, addToLikedArtists, addToRout, getRout, isAlbumLiked, likedArtists, removeFromLikedAlbums, removeFromLikedArtists } from "..";
+
+let selectedAlbum : Root2;
+let selectedArtist : string;
+
+
+function createExitSection (previousData? : any){
+    let html = `<section class="exit-section">
+    </section>`;
+    let iconHTML = `<i id="exit-info-page" class="fa fa-angle-left"></i>`;
+    let iconElement = htmlToElement(iconHTML);
+    iconElement.addEventListener('click', function(){
+        let func : Function = getRout();
+        func();
+    });
+    let sectionElement = htmlToElement(html);
+    sectionElement.appendChild(iconElement);
+    return sectionElement;
+}
+
+function createHeaderSection (album : Album){
+    let html = `<section class="header-section">
+    <img class="header-section__img-album" src="${album.album_thumb}" alt="">
+    </section>`;
+    let iconHTML = `<i class="fa fa-play-circle play-icon"></i>`;
+    let iconElement = htmlToElement(iconHTML);
+    iconElement.addEventListener('click', function(){
+        addToRout(createAlbumPage);
+        createPlayPage(getAlbumSongs(album.id)!, 0, false);
+    });
+    let sectionElement = htmlToElement(html);
+    sectionElement.appendChild(iconElement);
+    return sectionElement;
+}
+
+function getAlbumSongs (album : string){
+    for ( let song of songs){
+        if ( song.album.id == album){
+            return song.musics;
+        }
+    }
+    return null;
+}
+
+function createArtistHeaderSection (name : string, data : Root){
+    let pic = "https://upload.wikimedia.org/wikipedia/commons/a/ac/Default_pfp.jpg";
+    let html = `<section class="header-section">
+    <img class="header-section__img" src="${pic}" alt="">
+    </section>`;
+    let iconHTML = `<i class="fa fa-play-circle play-icon"></i>`;
+    let iconElement = htmlToElement(iconHTML);
+    iconElement.addEventListener('click', function(){
+        addToRout(createArtistPage);
+        createPlayPage(findArtistMusics(data, name), 0, false);
+    });
+    let sectionElement = htmlToElement(html);
+    sectionElement.appendChild(iconElement);
+    return sectionElement;
+}
+
+function createNameSection (album : Album){
+    let html = `<section class="name-section">
+    <div>
+        <h2 class="album-name">${album.album_name}</h2>
+        <h2 class="album-composer">${album.album_composer}</h2>
+    </div>
+    </section>`;
+    let iconHTML = `<i class="fa fa-heart like-album"></i>`;
+    let iconElement = htmlToElement(iconHTML);
+    if ( isAlbumLiked(album)){
+        iconElement.classList.add('button-clicked');
+    }
+    iconElement.addEventListener('click', function(){
+        if ( iconElement.classList.contains('button-clicked')){
+            removeFromLikedAlbums(album);
+        }
+        else{
+            addToLikedAlbums(album);
+        }
+        iconElement.classList.toggle('button-clicked')
+    });
+    let sectionElement = htmlToElement(html);
+    sectionElement.appendChild(iconElement);
+    return sectionElement;
+}
+
+function createArtistNameSection (name : string){
+    let html = `<section class="name-section">
+    <div>
+        <h2 class="album-name">${name}</h2>
+        <h2 class="album-composer">Artist</h2>
+    </div>
+    </section>`;
+    let iconHTML = `<i class="fa fa-heart like-album"></i>`;
+    let iconElement = htmlToElement(iconHTML);
+    if ( likedArtists.includes(name)){
+        iconElement.classList.add('button-clicked');
+    }
+    iconElement.addEventListener('click', function(){
+        if ( iconElement.classList.contains('button-clicked')){
+            removeFromLikedArtists(name);
+        }
+        else{
+            addToLikedArtists(name);
+        }
+        iconElement.classList.toggle('button-clicked')
+    });
+    let sectionElement = htmlToElement(html);
+    sectionElement.appendChild(iconElement);
+    return sectionElement;
+}
+
+function createSongListSection (albumTracks : Music[]){
+    let html = `<section class="songs-list-section"></section>`;
+    let sectionElement = htmlToElement(html);
+    for ( let song of albumTracks){
+        sectionElement.appendChild(createSongListItem(albumTracks, song));
+    }
+    return sectionElement;
+}
+
+function createArtistSongListSection (albumTracks : Music[]){
+    let html = `<section class="songs-list-section"></section>`;
+    let sectionElement = htmlToElement(html);
+    for ( let song of albumTracks){
+        sectionElement.appendChild(createSongListItem(albumTracks ,song));
+    }
+    return sectionElement;
+}
+
+function findArtistMusics (data : Root, artist : string){
+    let results : Music[] = []; 
+    for ( let album of data){
+        if ( album.album.album_composer === artist){
+            results = results.concat(album.musics);
+        }
+    }
+    return results;
+
+}
+
+function createSongListItem (songs : Music[], song : Music){
+    let html = `<div class="list__item">
+    <img class="list__item__img" src="${song.track_thumb}" alt="">
+    <p class="list__item__name">${song.track_name}</p>
+    </div>`;
+    let itemElement = htmlToElement(html);
+    itemElement.addEventListener('click', function(){
+        addToRout(createAlbumPage)
+        createPlayPage(songs, getSongIndex(songs, song)!, false);
+    });
+    return itemElement;
+}
+
+export function createAlbumPage (data? : Root2) {
+    if ( data != null){
+        selectedAlbum = data;
+    }
+    window.history.replaceState('', 'Album', 'album#' + selectedAlbum.album.id);
+    const body = document.getElementById('main-body')!;
+    body.innerHTML = '';
+    body.classList.remove('library-main-body');
+    body.classList.add('album-main-body');
+    body.appendChild(createExitSection());
+    body.appendChild(createHeaderSection(selectedAlbum.album));
+    body.appendChild(createNameSection(selectedAlbum.album));
+    body.appendChild(createSongListSection(selectedAlbum.musics));
+    setRequestedColor(selectedAlbum.album.album_thumb);
+}
+
+export function createArtistPage (data? : string) {
+    if ( data != null){
+        selectedArtist = data;
+    }
+    window.history.replaceState('', 'Artist', 'artist#' + selectedArtist)
+    const body = document.getElementById('main-body')!;
+    body.innerHTML = '';
+    body.classList.remove('library-main-body');
+    body.classList.add('album-main-body');
+    body.appendChild(createExitSection());
+    body.appendChild(createArtistHeaderSection(selectedArtist, songs));
+    body.appendChild(createArtistNameSection(selectedArtist));
+    body.appendChild(createArtistSongListSection(findArtistMusics(songs, selectedArtist)));
+}
+
---
+++ Components/HomeComponents.ts
@@ -0,0 +1,332 @@
+import { addToRecentlyPlayed, addToRout, artistExists, getSong, recentlyPlayed, routingTracked } from '..';
+import { Album, Music, Root, Root2 } from '../data/SongFormatter';
+import songs from '../data/songs.json';
+import { createAlbumPage, createArtistPage } from './AlbumPageComponents';
+import { createLibraryPage } from './LibraryComponents';
+import { createPlayPage, stopSong } from './PlayComponents';
+import { createSearchPage } from './SearchComponents';
+
+type Pair = {
+    n1 : number;
+    n2 : number;
+}
+
+export function htmlToElement(html : string) : HTMLScriptElement{
+    var template = document.createElement('template');
+    html = html.trim();
+    template.innerHTML = html;
+    return <HTMLScriptElement>template.content.firstChild!;
+}
+
+function createNavbar (){
+    const navBarHTML : string = `
+    <nav class="menu">
+        <div class="menu-item">
+            <i class="menu-item__icon fa fa-home selected"></i>
+            <p class="menu-item__label">Home</p>
+        </div>
+        <div class="menu-item">
+            <i class="menu-item__icon fa fa-search "></i>
+            <p class="menu-item__label">Search</p>
+        </div>
+        <div class="menu-item">
+            <i class="menu-item__icon fa fa-bookmark "></i>
+            <p class="menu-item__label">Your Library</p>
+        </div> 
+    </nav>
+    `;
+    document.getElementById('view-point')?.appendChild(htmlToElement(navBarHTML));
+}
+
+function setNavbarLocating (){
+    let navs = document.querySelectorAll('.menu-item');
+    let icons = document.querySelectorAll('.menu-item__icon')
+    navs[0].addEventListener('click', function(){
+        createHomePage();
+        icons[0].classList.add('selected');
+        icons[1].classList.remove('selected');
+        icons[2].classList.remove('selected');
+    });
+    navs[1].addEventListener('click', function(){
+        createSearchPage();
+        icons[1].classList.add('selected');
+        icons[0].classList.remove('selected');
+        icons[2].classList.remove('selected');
+    });
+    navs[2].addEventListener('click', function(){
+        createLibraryPage();
+        icons[2].classList.add('selected');
+        icons[1].classList.remove('selected');
+        icons[0].classList.remove('selected');
+    });
+}
+
+let suggestedsPlaylist : Music[] = [];
+
+function createSuggestedSection (songs : Root){
+    suggestedsPlaylist = [];
+    const sectionHTML = `
+    <section class="main-container__suggested home__section">
+        <h2 class="section-header">Suggested</h2>
+    </section>`;
+    const gridContainer = htmlToElement('<div id = "suggested-grid" class="suggested-grid"></div>'); 
+    const section = htmlToElement(sectionHTML);
+    let suggesteds : Pair[] = [];
+    while (suggesteds.length < 6){
+        let rand = Math.floor(Math.random() * songs.length);
+        let rand2 = Math.floor(Math.random() * songs[rand].musics.length);
+        let newPair : Pair= {
+            n1 : rand,
+            n2 : rand2
+        } 
+        if ( !contains(suggesteds, newPair)){
+            suggesteds.push(newPair);
+            suggestedsPlaylist.push(songs[rand].musics[rand2]);
+            gridContainer.appendChild(createSuggestedItem(songs[rand].musics[rand2]))
+        }
+    }
+    section.appendChild(gridContainer);
+    return section;
+}
+
+function contains (list : Pair[], target : Pair) : boolean{
+    for ( let pair of list){
+        if ( pair.n1 == target.n1 && pair.n2 == target.n2){
+            return true;
+        }
+    }
+    return false;
+}
+
+function createSuggestedItem (song : Music){
+    let songThumb : string = song.track_thumb;
+    if (songThumb == null || songThumb == ""){
+         songThumb = "../public/pics/no_cover.jpg";
+    }
+    let html = `
+    <div id = "${"s" +  song.id}"class="grid-item">
+        <img loading="lazy" class="suggested-item__pic" src="${songThumb}" alt="">
+        <p class="suggested-item__name">${song.track_name}</p>
+    </div>
+    `;
+    let element : HTMLScriptElement = htmlToElement(html);
+    element.addEventListener('click', function(){
+        addToRout(createHomePage);
+        createPlayPage (suggestedsPlaylist, suggestedsPlaylist.indexOf(song), false);
+    })
+    return element;
+}
+
+
+
+function createRecentlyItem(song : Music, index : number){
+    let html = `<div id = "${song.id}" class="recently-played__item">
+    <img loading="lazy" class="recently-played__item-img" src="${song.track_thumb}" alt="">
+    <p class="recently-played__item-name">${song.track_name}</p>
+    </div>`;
+    let element : HTMLScriptElement = htmlToElement(html);
+    element.addEventListener('click', function(){
+        addToRout(createHomePage);
+        createPlayPage(recentlyPlayed.slice(0, 15), index, false)
+    })
+    return element;
+}
+
+function createRecentlySection (songs : Root){
+    // just for test 
+    let sectionHTML = `<section class="main-container__recently-played home__section">
+    <h2 class="section-header">Recently Played</h2>
+    </section> `;
+    let container = htmlToElement(`<div id="recently-played__content-container" class="scroll-container">
+    </div>`);
+    let counter = 0;
+    for (let item of recentlyPlayed){
+        container.appendChild(createRecentlyItem(item, counter));
+        counter++;
+        if (counter == 7){
+            break;
+        }
+    }
+    let section = htmlToElement(sectionHTML);
+    section.appendChild(container);
+    return section;
+}
+
+function createNewReleasesItem (song : Album){
+    let songThumb : string = song.album_thumb;
+    if (songThumb == null || songThumb == ""){
+         songThumb = "../public/pics/no_cover.jpg";
+    }
+    let html = `<div id = "${song.id}" class="new-releases__item">
+    <img loading="lazy" class="new-releases__item-pic" src="${songThumb}" alt="">
+    </div>`;
+    let element : HTMLScriptElement = htmlToElement(html);
+    element.addEventListener('click', function(){
+        addToRout(createHomePage);
+        createAlbumPage(getAlbumData(element.id)!);
+    })
+    return element;
+}
+
+export function getAlbumData (id : string) : Root2 | null{
+    for (let data of songs){
+        if (data.album.id == id){
+            return data;
+        }
+    }
+    return null;
+}
+
+function createNewReleasesSection (songs : Root){
+    let sectionHTML = `<section class="main-container__new-releases home__section">
+    <h2 class="section-header">New Releases</h2>
+    </section>  `;
+    let gridContainer = htmlToElement(`<div id="new-releases__content-container" class="scroll-container new-releases">
+    </div>`);
+    for ( let i = 0 ; i < 6 ; i++){
+        gridContainer.appendChild(createNewReleasesItem(songs[i].album));
+    }
+    let section = htmlToElement(sectionHTML);
+    section.appendChild(gridContainer);
+    return section;
+}
+
+function createMainBody(){
+    let html = `<div id = "main-body" class="main-body"></div>`;
+    document.getElementById('view-point')?.appendChild(htmlToElement(html));
+}
+
+
+export function createHomePage (){
+    history.replaceState('', 'Home', '/home');
+    const body = document.getElementById('main-body')!;
+    body.innerHTML = '';
+    body.classList.remove('library-main-body');
+    body.classList.remove('album-main-body');
+    body.appendChild(createSuggestedSection(songs));
+    body.appendChild(createRecentlySection(songs));
+    body.appendChild(createNewReleasesSection(songs));
+    setDefaultColor();
+}
+
+function locationHandler() : boolean{
+    const url = window.location;
+    const hash = url.hash.replace("#", "");
+    switch(url.pathname){
+        case '/search' :
+            createSearchPage();
+            return true;
+        case '/library' :
+            createLibraryPage();
+            return true;
+
+        case '/album' :
+            if ( !getAlbumData(hash)){
+                return false;
+            }
+            createAlbumPage(getAlbumData(hash)!);
+            return true;
+        case '/artist' :
+            if ( !artistExists(hash)){
+                return false;
+            } 
+            createArtistPage(hash);
+            return true;
+        case '/play' :
+            let obj = getSong(hash);
+            if ( !obj){
+                return false;
+            }
+            createPlayPage(obj.list, obj.index, false);
+            return true;
+    }
+    return false;
+}
+
+export function setup (){
+    createMainBody();
+    createMiniPlayerContainer();
+    createNavbar();
+    setNavbarLocating();
+    if ( locationHandler()){
+        addToRout(createHomePage);
+        return;
+    }
+    createHomePage();
+}
+
+function createMiniPlayerContainer() {
+    let html = `<div id = "mini-player-container" class="mini-player-container"></div>`;
+    document.getElementById('view-point')?.appendChild(htmlToElement(html));
+}
+
+export function getAverageRgb(src : string) {
+    var context = document.createElement('canvas').getContext('2d')!;
+    let image = new Image;
+    image.setAttribute('crossOrigin', ''); 
+    image.src = src;
+    context.imageSmoothingEnabled = true;
+    context.drawImage(image, 0, 0, 1, 1);
+    return context.getImageData(0, 0, 1, 1).data.slice(0,3);
+}
+
+export function setRequestedColor (src : string){
+    //let input = getAverageRgb(src);
+    getAverageRGB(src);
+
+}
+
+function getAverageRGB(src : string) {
+    let imgEl = document.createElement('img');
+    imgEl.src = src;
+    var blockSize = 5, // only visit every 5 pixels
+        defaultRGB = {r:0,g:0,b:0}, // for non-supporting envs
+        canvas = document.createElement('canvas'),
+        context = canvas.getContext && canvas.getContext('2d'),
+        data, width, height,
+        i = -4,
+        length,
+        rgb = {r:0,g:0,b:0},
+        count = 0;
+
+    if (!context) {
+        return defaultRGB;
+    }
+
+    height = canvas.height = imgEl.naturalHeight || imgEl.offsetHeight || imgEl.height;
+    width = canvas.width = imgEl.naturalWidth || imgEl.offsetWidth || imgEl.width;
+
+    context.drawImage(imgEl, 0, 0);
+
+    try {
+        data = context.getImageData(0, 0, width, height);
+    } catch(e) {
+        /* security error, img on diff domain */
+        return defaultRGB;
+    }
+
+    length = data.data.length;
+
+    while ( (i += blockSize * 4) < length ) {
+        ++count;
+        rgb.r += data.data[i];
+        rgb.g += data.data[i+1];
+        rgb.b += data.data[i+2];
+    }
+
+    // ~~ used to floor values
+    rgb.r = ~~(rgb.r/count);
+    rgb.g = ~~(rgb.g/count);
+    rgb.b = ~~(rgb.b/count);
+
+    console.log('hi');
+    let rt = document.documentElement;
+    let color = `rgb(${rgb.r}, ${rgb.g}, ${rgb.b})`
+    rt.style.setProperty("--selected-text-color", color);
+
+}
+
+export function setDefaultColor (){
+    let rt = document.documentElement;
+    rt.style.setProperty("--selected-text-color", 'rgb(0, 107, 18)');
+}
